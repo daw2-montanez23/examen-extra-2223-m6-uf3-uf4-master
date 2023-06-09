b604d56f331b38cb29ecd632eda18f8daff5b0c5
daw2-montanez23 <francescmontanez2002@gmail.com>
2023-06-09 17:03:31 +0200

    Retoques

--- src/componentes/header.js
+++ src/componentes/header.js
@@ -1,11 +1,12 @@
 import login from "../vistas/login"
 import registro from "../vistas/registro"
+import { HomeComponent } from "./homeComponent"
 
 export const header = {
     template : `
     <div class="navbar navbar-dark bg-dark">
       <div class="container-fluid">
-        <span class="navbar-brand mb-0">2223 M6 UF3/UF4 - Examen extraordinaria - Alumnno:Francesc Monañez Rosell</span>
+        <span id="homeComponent" class="navbar-brand mb-0">2223 M6 UF3/UF4 - Examen extraordinaria - Alumnno:Francesc Monañez Rosell</span>
         <form  class="d-flex">
           <input class="form-control me-2" placeholder="email@email.com">
           <button id="login" class="btn btn-outline-success" >Login</button>
@@ -27,6 +28,12 @@ export const header = {
         e.preventDefault()
         document.querySelector('main').innerHTML = registro.template
         registro.script()
+      })
+
+      document.querySelector('#homeComponent').addEventListener("click", (e)=>{
+        e.preventDefault()
+        document.querySelector('main').innerHTML = login.template
+        login.script()
       })
     }
 }
