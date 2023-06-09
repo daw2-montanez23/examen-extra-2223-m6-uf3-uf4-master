c29c8615a7c917586e65736d636f2a84060c5c98
daw2-montanez23 <francescmontanez2002@gmail.com>
2023-06-09 17:03:50 +0200

    Updates

--- assets/main-8be1573f.js
+++ assets/main-8be1573f.js
@@ -67,33 +67,6 @@ const registro = {
                 </form>
             </div>`
 };
-const header = {
-  template: `
-    <div class="navbar navbar-dark bg-dark">
-      <div class="container-fluid">
-        <span class="navbar-brand mb-0">2223 M6 UF3/UF4 - Examen extraordinaria - Alumnno:Francesc Monañez Rosell</span>
-        <form  class="d-flex">
-          <input class="form-control me-2" placeholder="email@email.com">
-          <button id="login" class="btn btn-outline-success" >Login</button>
-          <button id="registro" class="btn btn-link" >Regístrate</button>
-        </form>
-        
-      </div>
-    </div>
-    `,
-  script: async () => {
-    document.querySelector("#login").addEventListener("click", (e) => {
-      e.preventDefault();
-      document.querySelector("main").innerHTML = login.template;
-      login.script();
-    });
-    document.querySelector("#registro").addEventListener("click", (e) => {
-      e.preventDefault();
-      document.querySelector("main").innerHTML = registro.template;
-      registro.script();
-    });
-  }
-};
 const series = [
   {
     "nombre": "Stranger Things",
@@ -192,6 +165,38 @@ function generarSeriesCards(series2) {
   }
   return cards;
 }
+const header = {
+  template: `
+    <div class="navbar navbar-dark bg-dark">
+      <div class="container-fluid">
+        <span id="homeComponent" class="navbar-brand mb-0">2223 M6 UF3/UF4 - Examen extraordinaria - Alumnno:Francesc Monañez Rosell</span>
+        <form  class="d-flex">
+          <input class="form-control me-2" placeholder="email@email.com">
+          <button id="login" class="btn btn-outline-success" >Login</button>
+          <button id="registro" class="btn btn-link" >Regístrate</button>
+        </form>
+        
+      </div>
+    </div>
+    `,
+  script: async () => {
+    document.querySelector("#login").addEventListener("click", (e) => {
+      e.preventDefault();
+      document.querySelector("main").innerHTML = login.template;
+      login.script();
+    });
+    document.querySelector("#registro").addEventListener("click", (e) => {
+      e.preventDefault();
+      document.querySelector("main").innerHTML = registro.template;
+      registro.script();
+    });
+    document.querySelector("#homeComponent").addEventListener("click", (e) => {
+      e.preventDefault();
+      document.querySelector("main").innerHTML = login.template;
+      login.script();
+    });
+  }
+};
 console.log("hola");
 console.log(series);
 document.querySelector("header").innerHTML = header.template;
